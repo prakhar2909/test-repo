b935ef4b67f2f8f4939cc9956c2f17e143d09a7e
prakhar2909 <01prakharsrivastava@gmail.com>
2025-12-04 10:09:22 +0530

    Add main App component with routing

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
